3eff172ee802fdc515f78d241e213ade11218bae
Klaus Aschenbrenner <klaus.aschenbrenner@sqlpassion.at>
2025-08-11 12:19:32 +0200

    A simple RegEx-based tokenizer.

--- llm/llm.py
+++ llm/llm.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import re
 
+# Read a sample text from the file system
 script_dir = Path(__file__).resolve().parent
 file_path = script_dir / "The_Verdict.txt"
 
@@ -7,3 +9,22 @@ with file_path.open("r", encoding="utf-8") as file:
      raw_text = file.read()
 print("Total number of characters: ", len(raw_text))
 print(raw_text[:100])  # Display the first 100 characters to check the content
+
+# Tokenize the text through Regular Expressions
+preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+print("Total number of tokens: ", len(preprocessed))
+print(preprocessed[:30])
+
+# Create the vocabulary and assign to each unique word a token ID
+all_words = sorted(set(preprocessed))
+vocabulary_size = len(all_words)
+vocabulary = {token:integer for integer, token in enumerate(all_words)}
+print("Vocabulary size: ", vocabulary_size)
+
+# Print out the first 50 entries with their token IDs
+for i, item in enumerate(vocabulary.items()):
+     print(item)
+
+     if i >= 50:
+          break
